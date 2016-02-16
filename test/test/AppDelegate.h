be573082f7cfac49dc9835aac7468f6581c3cf52
zdx <18259129536@163.com>
2016-02-16 16:07:35 +0800

    叼叼叼
    叼叼叼

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 郑东喜 on 16/2/16.
+//  Copyright © 2016年 郑东喜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
